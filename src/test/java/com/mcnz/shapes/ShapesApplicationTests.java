dfa978eeaf4271736027be4565fe0e6aa032d31c
Jimbo James <jimbo@james.com>
2026-09-14 09:56:40 -0700

    stuff

---
+++ src/test/java/com/mcnz/shapes/ShapesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcnz.shapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
